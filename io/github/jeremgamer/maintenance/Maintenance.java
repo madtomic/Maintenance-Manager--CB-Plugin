abfcd995f6ada09d0f55193920649daf577fda70
JeremGamer <jeremking.l@hotmail.fr>
2014-05-06 17:26:27 +0200

    Update Maintenance.java

--- io/github/jeremgamer/maintenance/Maintenance.java
+++ io/github/jeremgamer/maintenance/Maintenance.java
@@ -1,5 +1,11 @@
 package io.github.jeremgamer.maintenance;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -11,6 +17,7 @@ import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.CachedServerIcon;
 
 public final class Maintenance extends JavaPlugin implements Listener {
 	
@@ -22,17 +29,19 @@ public final class Maintenance extends JavaPlugin implements Listener {
     String durationArgument;
     private Thread t;
     private Thread t2;
-
-	
+    
+    
     
     @Override
     public void onEnable() {
     	getServer().getPluginManager().registerEvents(this, this);
+    	maintenanceTime = getConfig().getBoolean("maintenanceModeOnStart");
+        this.saveDefaultConfig();
     }
  
     @Override
     public void onDisable() {
-
+    	this.saveConfig();
     }
 
     @Override
@@ -42,33 +51,40 @@ public final class Maintenance extends JavaPlugin implements Listener {
     		if (args[0].equalsIgnoreCase( "on" )) {
         		if (args.length == 2) {
         			scheduleArgument = args[1];
-        			t = new Thread(new MaintenanceSchedule(this));
+        			t = new Thread(new MaintenanceSchedule(this, sender));
         			t.start();
         			durationArgument = "0";
-        			t2 = new Thread(new MaintenanceDuration(this));
+        			t2 = new Thread(new MaintenanceDuration(this, sender));
         			t2.start();
+        			this.getConfig().set("maintenanceModeOnStart", false);
         	        return true;
         	        } 
         		if (args.length == 3) {
         			scheduleArgument = args[1];
-        			t = new Thread(new MaintenanceSchedule(this));
+        			t = new Thread(new MaintenanceSchedule(this, sender));
         			t.start();
         			durationArgument = args[2];
-        			t2 = new Thread(new MaintenanceDuration(this));
+        			t2 = new Thread(new MaintenanceDuration(this, sender));
         			t2.start();
+        			this.getConfig().set("maintenanceModeOnStart", false);
         			return true;
         		}
     			maintenanceTime = true;
-    			Bukkit.getServer().broadcastMessage( "Maintenance time!" );
+    			this.getConfig().set("maintenanceModeOnStart", true);
+    			Bukkit.getServer().broadcastMessage( getConfig().getString("maintenanceStart") );
     			for (Player player: Bukkit.getServer().getOnlinePlayers()) {
     				if ( !player.hasPermission( "maintenance.access" ) || !player.isOp() ) {
-    					player.kickPlayer( "The server is currently under maintenance. Come back later." );
+    					player.kickPlayer( getConfig().getString("kickMessage") );
     				}
     			}
     			return true;
     		} else if (args[0].equalsIgnoreCase( "off" )) {
     			maintenanceTime = false;
-    			Bukkit.getServer().broadcastMessage( "Maintenance finished!" );
+    			this.getConfig().set("maintenanceModeOnStart", false);
+    			Bukkit.getServer().broadcastMessage( getConfig().getString("maintenanceEnd") );
+    			return true;
+    		} else if (args[0].equalsIgnoreCase( "reload" )) {
+    			this.reloadConfig();
     			return true;
     		}
     	}
@@ -79,7 +95,7 @@ public final class Maintenance extends JavaPlugin implements Listener {
     public void onLogin (final PlayerLoginEvent event) {
     	Player player = event.getPlayer();
     	if (maintenanceTime == true && !player.isOp()) {    		
-    			event.disallow( org.bukkit.event.player.PlayerLoginEvent.Result.KICK_OTHER, "The server is currently under maintenance. Come back later." );
+    			event.disallow( org.bukkit.event.player.PlayerLoginEvent.Result.KICK_OTHER, getConfig().getString("maintenanceMessage") );
     			return;
     	} else if (maintenanceTime == true && player.hasPermission( "maintenance.acess" )) {
     			event.allow();
@@ -90,17 +106,20 @@ public final class Maintenance extends JavaPlugin implements Listener {
     	
         @SuppressWarnings("unused")
 		private final JavaPlugin plugin;
+        private final CommandSender sender;
         
-        public MaintenanceSchedule(JavaPlugin plugin) {
+        public MaintenanceSchedule(JavaPlugin plugin, CommandSender sender) {
             this.plugin = plugin;
+            this.sender = sender;
         }
      
         @SuppressWarnings("static-access")
 		@Override
         public void run() {
-    		schedule = Integer.parseInt(scheduleArgument);
-    		Bukkit.getServer().broadcastMessage( "Maintenance time in " + schedule + " minutes!");
-    		scheduleM = Integer.parseInt(scheduleArgument) * 60 * 1000;
+        	try{
+        		schedule = Integer.parseInt(scheduleArgument);
+        		Bukkit.getServer().broadcastMessage( "Maintenance time in " + schedule + " minutes!");
+        		scheduleM = Integer.parseInt(scheduleArgument) * 60 * 1000;
     		try {
 				t.sleep(scheduleM / 2);
 			} catch (InterruptedException e) {
@@ -112,43 +131,62 @@ public final class Maintenance extends JavaPlugin implements Listener {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-    		Bukkit.getServer().broadcastMessage( "Maintenance time!" );
+    		Bukkit.getServer().broadcastMessage( getConfig().getString("maintenanceStart") );
     		for (Player player: Bukkit.getServer().getOnlinePlayers()) {
     			if ( !player.hasPermission( "maintenance.access" ) || !player.isOp() ) {
-    				player.kickPlayer( "The server is currently under maintenance. Come back later." );;
+    				player.kickPlayer( getConfig().getString("kickMessage") );;
     			}
     		}
-     
+    		} catch (NumberFormatException e){
+    			sender.sendMessage( getConfig().getString("inputErrorSchedule") );
+    		}
         }
     }
     
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void changeMOTD (final ServerListPingEvent event){
+    public void changeMOTD (final ServerListPingEvent event) {
     	if ( maintenanceTime == true ) {
-    		event.setMotd( "Maintenance mode..." );
-    	}
+    		event.setMotd( getConfig().getString("maintenanceMOTD") );
+			try {
+				BufferedImage maintenanceImage = ImageIO.read(new URL( getConfig().getString("maintenanceIcon") ));
+				CachedServerIcon maintenanceIcon = Bukkit.getServer().loadServerIcon(maintenanceImage);
+	    		event.setServerIcon(maintenanceIcon); 
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+    	}   	
     }
     
     public class MaintenanceDuration extends BukkitRunnable {
     	
         @SuppressWarnings("unused")
 		private final JavaPlugin plugin;
+        private final CommandSender sender;
         
-        public MaintenanceDuration(JavaPlugin plugin) {
+        public MaintenanceDuration(JavaPlugin plugin, CommandSender sender) {
             this.plugin = plugin;
+            this.sender = sender;
         }
      
         @SuppressWarnings("static-access")
 		@Override
         public void run() {
-        	durationM = Integer.parseInt(durationArgument) * 60 * 1000;
+        	try{
+        		durationM = Integer.parseInt(durationArgument) * 60 * 1000;
         	try {
 				t2.sleep(durationM);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			maintenanceTime = false;
-			Bukkit.getServer().broadcastMessage( "Maintenance finished!" );
+			Bukkit.getServer().broadcastMessage( getConfig().getString("maintenanceEnd") );
+        	} catch (NumberFormatException e){
+    			sender.sendMessage( getConfig().getString("inputErrorDuration") );
+        	}
         }
     }
 }
